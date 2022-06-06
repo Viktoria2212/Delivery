aee257769eb342f3d2833468ff722e877a1773ee
Viktoria2212 <zyablikova.vika@mail.ru>
2022-06-06 15:33:25 +0300

    Before and After annotations have been added

--- src/test/java/ru/deliveryClub/FirstTest.java
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -1,20 +1,30 @@
 package ru.deliveryClub;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
+
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/Users/nina.belova/Downloads/chromedriver_win32/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
     @Test
     public void firstTest() {
-        System.setProperty("webdriver.chrome.driver", "/Users/nina.belova/Downloads/chromedriver_win32/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-
         driver.get("https://www.delivery-club.ru/");
 
         String title = driver.getTitle();
         Assert.assertEquals("Delivery Club — Доставка еды и продуктов", title);
+    }
 
+    @After
+    public void close() {
         driver.quit();
     }
 }
